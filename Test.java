16b1a517153f86c82f1ba4ddf66c6c423355bf41
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-09-20 23:20:53 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+class Test{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=40;
+        int sum=a+b;
+        float num = 5.2f;
+        double num1 = 9.5;
+        String user="Pream";
+        System.out.println(sum);
+        System.out.println(num+num1);
+        System.out.println("name:"+user);
+    }
+}
