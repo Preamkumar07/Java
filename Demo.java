4a71229bf5cdabe8030550746f76d165fd0ff42b
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-09-21 19:35:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,17 @@
+class Demo{
+    public static void main(String[] args) {
+        byte b = 127;
+         int a = b ;
+        System.out.println(a);
+        int c = 20;
+        byte k = (byte)c ;
+        float f = 5.6f;
+        int n = (int)f;
+        System.out.println(n);
+        System.out.println(k);
+        byte m = 10;
+        byte v = 30;
+        int result = m*v ;
+        System.out.println(result);
+    }
+} 
