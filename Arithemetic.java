21a98fb95f1a7784987333ca3558b6473f0a4d69
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-09-21 22:31:31 +0530

    Add files via upload

---
+++ Arithemetic.java
@@ -0,0 +1,12 @@
+class Arithemetic{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum =a+b;
+        a++;
+        int product =a*b;
+        System.out.println(sum);
+        System.out.println(a);
+        System.out.println(product);
+    }
+}
---
+++ Relational.java
@@ -0,0 +1,20 @@
+class Relational{
+    public static void main(String[] args) {
+        int x=6;
+        int y=5;
+        double i = 8.5 ;
+        double j = 9.8 ;
+        boolean result = x > y ;
+        boolean a= x >= y ;
+        boolean b = i <= j ;
+        boolean c = x == y ;
+        boolean d = i < j ;
+        boolean e = i != j ;
+        System.out.println(e);
+        System.out.println(result);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
